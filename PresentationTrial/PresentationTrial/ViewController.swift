e8ca6defc4e53ce7837c7b94774808c0931f0892
AmanGuptaZ <30328911+AmanGuptaZ@users.noreply.github.com>
2017-07-26 11:17:49 +1200

    Text Wrapping

--- PresentationTrial/PresentationTrial/ViewController.swift
+++ PresentationTrial/PresentationTrial/ViewController.swift
@@ -15,21 +15,19 @@ class ViewController: UIViewController {
     
     @IBAction func button(_ sender: UIButton) {
     
-        labelToChange.text = "Some text"
+        labelToChange.preferredMaxLayoutWidth = 150
+        labelToChange.text = "Some text has been displayed in this presentation to make sure that the label is now expandable"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("hello")
+        print("This seems to be working")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    
 }
-

c93ddcaccf482a64f965dc89c42c6855219a4c99
AmanGuptaZ <30328911+AmanGuptaZ@users.noreply.github.com>
2017-07-26 10:46:50 +1200

    label title change on button pressed

--- PresentationTrial/PresentationTrial/ViewController.swift
+++ PresentationTrial/PresentationTrial/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var labelToChange: UILabel!
+    
+    
+    @IBAction func button(_ sender: UIButton) {
+    
+        labelToChange.text = "Some text"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hello")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
